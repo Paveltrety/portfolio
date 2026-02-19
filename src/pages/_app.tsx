84efab2bf21638cb7fee4cabc33e2208a93a1376
Pavel Tretyakov <paveltretyakov@MacBook-Pro-Pavel-2.local>
2026-02-19 23:26:46 +0700

    add new font and init yandex metrik

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,15 @@ import '@/styles/styles.scss';
 import '@/styles/globals.scss';
 
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import { Geist } from 'next/font/google';
 import { Provider } from 'react-redux';
 
 import { Layout } from '@/components/Layout/Layout';
 import { withStaticI18n } from '@/i18n/withStaticI18n';
 import store from '@/store';
 
-const inter = Inter({
-  variable: '--font-inter',
+const geist = Geist({
+  variable: '--font-geist',
   subsets: ['latin'],
   display: 'swap',
 });
@@ -20,7 +20,7 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <style jsx global>{`
         html {
-          font-family: ${inter.style.fontFamily};
+          font-family: ${geist.style.fontFamily};
         }
       `}</style>
       <Provider store={store}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { YandexMetrikaScript } from '@/analytics/YandexMetrika';
+
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <YandexMetrikaScript />
+      </Head>
       <body>
         <Main />
         <NextScript />
